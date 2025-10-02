6b65f6ae5b120ff103c5003e4bf6fd61bf5865f0
FranseFrikandel <romain@ceracarbon-racing.com>
2025-10-02 19:13:27 +0200

    Small stylistic changes and preperations for using releases

--- generate_chart.py
+++ generate_chart.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
-from labellines import labelLine, labelLines
+from labellines import labelLines
 
 low_rpm = 10
 high_rpm = 2000
@@ -15,8 +15,8 @@ surf_speeds = np.array([50, 100, 150, 200, 250, 300])
 spindle_rpm = np.linspace(low_rpm, high_rpm, 100)
 fig, ax = plt.subplots(figsize=((297 - 20) / 25.4, (210 - 20) / 25.4))
 
-ax.set_xlim(low_rpm, high_rpm)
-ax.set_ylim(low_diameter, high_diameter)
+ax.set_xlim(0, high_rpm)
+ax.set_ylim(0, high_diameter)
 
 ax.xaxis.set_major_locator(MultipleLocator(200))
 ax.xaxis.set_minor_locator(MultipleLocator(50))
@@ -37,11 +37,11 @@ for speed in surf_speeds:
 
 labelLines(ax.get_lines(), zorder=50)
 
-plt.figtext(0.23, 0.04, "Recommended speeds:", wrap=True, horizontalalignment="left", fontsize=10)
-plt.figtext(0.4, 0.04, "Aluminium 150-250 m/min", wrap=True, horizontalalignment="left", fontsize=10)
-plt.figtext(0.6, 0.04, "Steel 90-150 m/min", wrap=True, horizontalalignment="left", fontsize=10)
-plt.figtext(0.4, 0.02, "Stainless steel 45-75 m/min", wrap=True, horizontalalignment="left", fontsize=10)
-plt.figtext(0.6, 0.02, "Brass 120-250 m/min", wrap=True, horizontalalignment="left", fontsize=10)
+plt.figtext(0.25, 0.06, "Recommended speeds:", wrap=True, horizontalalignment="left", fontsize=10)
+plt.figtext(0.43, 0.06, "Aluminium 150-250 m/min", wrap=True, horizontalalignment="left", fontsize=10)
+plt.figtext(0.63, 0.06, "Steel 90-150 m/min", wrap=True, horizontalalignment="left", fontsize=10)
+plt.figtext(0.43, 0.04, "Stainless steel 45-75 m/min", wrap=True, horizontalalignment="left", fontsize=10)
+plt.figtext(0.63, 0.04, "Brass 120-250 m/min", wrap=True, horizontalalignment="left", fontsize=10)
 
 plt.tight_layout(rect=(0, 0.06, 1, 1))
 # plt.show()
